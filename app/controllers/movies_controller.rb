d284525d5df65b1090262ee97868eeb6b410a52d
Victor Emanuel Ramalho Ribeiro <verribeiro@hotmail.com>
2019-12-17 22:57:08 -0300

    adicionando search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,14 @@ class MoviesController < ApplicationController
   def now
     Tmdb::Api.key('76a5c60d4eb397b26f90d390c849ccbf')
     Tmdb::Api.language('pt-BR')
-    @now_playing = Tmdb::Movie.now_playing
+    @now_playing = Tmdb::Movie.find("batman")
   end
+
+  def search
+    Tmdb::Api.key('76a5c60d4eb397b26f90d390c849ccbf')
+    Tmdb::Api.language(params[:idioma])
+
+    @now_playing = Tmdb::Movie.find(params[:title])
+  end
+
 end
