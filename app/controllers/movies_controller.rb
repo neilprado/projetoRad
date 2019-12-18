61315f42e938398c4780730107b2925a57577661
IFPB <ifpb@MAC02LB04-JP.local>
2019-12-18 13:41:32 -0300

    Populares, lancamentos e pequenas correcoes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,17 +5,23 @@ class MoviesController < ApplicationController
     @now_playing = Tmdb::Movie.now_playing
   end
 
-  def now
+  def popular
     Tmdb::Api.key('76a5c60d4eb397b26f90d390c849ccbf')
     Tmdb::Api.language('pt-BR')
-    @now_playing = Tmdb::Movie.find("batman")
+    @popular = Tmdb::Movie.popular
+  end
+
+  def upcoming
+    Tmdb::Api.key('76a5c60d4eb397b26f90d390c849ccbf')
+    Tmdb::Api.language('pt-BR')
+    @upcoming = Tmdb::Movie.upcoming
   end
 
   def search
     Tmdb::Api.key('76a5c60d4eb397b26f90d390c849ccbf')
     Tmdb::Api.language(params[:idioma])
 
-    @now_playing = Tmdb::Movie.find(params[:title])
+    @search = Tmdb::Movie.find(params[:title])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :users
 
   get 'movies/search', to: "movies#search"
+  get 'movies/popular', to: "movies#popular"
+  get 'movies/upcoming', to: "movies#upcoming"
 
 end
