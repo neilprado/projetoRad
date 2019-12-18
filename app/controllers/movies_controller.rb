66493897766c12f38f34cc9a4789b29181e3b404
Victor Emanuel Ramalho Ribeiro <verribeiro@hotmail.com>
2019-12-17 21:20:12 -0300

    adicionando cards de filmes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,8 @@
 class MoviesController < ApplicationController
   def index
+    Tmdb::Api.key('76a5c60d4eb397b26f90d390c849ccbf')
+    Tmdb::Api.language('pt-BR')
+    @now_playing = Tmdb::Movie.now_playing
   end
 
   def now
