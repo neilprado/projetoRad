f8ae5a998a64629dcf450cf4e90813625c65410f
Victor Emanuel Ramalho Ribeiro <verribeiro@hotmail.com>
2019-12-18 21:50:14 -0300

    redirecionando pra pagina do filme

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,11 +24,11 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)    
+    @comment = Comment.new(comment_params)
     puts comment_params
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to "/movies/show?id=#{@comment.idFilme}", notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
